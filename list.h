5493eefd7826c73c842bfcbcb30a6fc8939081f4
DarthChurros <tnarayan00@stuy.edu>
2019-10-28 19:52:46 -0400

    added one more header

--- list.h
+++ list.h
@@ -4,6 +4,7 @@
 struct song { char name[100]; char artist[100]; struct song* next; };
 void print_list(struct song* list);
 struct song* insert_front(struct song* list, char* name, char* artist);
+struct song* insert_ordered(struct song* list, char* name, char* artist);
 struct song* find_song(struct song* list, char* name, char* artist);
 struct song* find_artist(struct song* list, char* artist);
 struct song* remove_song(struct song* list, struct song* target);
