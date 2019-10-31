234b5f8ebb835f76b68457a5313224f234d9e7d4
DarthChurros <tnarayan00@stuy.edu>
2019-10-31 11:45:31 -0400

    wrote tests for unordered/ordered add and free

--- main.c
+++ main.c
@@ -7,6 +7,34 @@ int main() {
     struct song* song_list = NULL;
 
     printf("\nAdding items to list...\n");
+    song_list = insert_front(song_list, "Runaway", "Kanye West");
+    song_list = insert_front(song_list, "Coldest Winter", "Kanye West");
+    song_list = insert_front(song_list, "Saint Pablo", "Kanye West");
+    song_list = insert_front(song_list, "Blood On The Leaves", "Kanye West");
+    song_list = insert_front(song_list, "Fluorescent Adolescent", "Arctic Monkeys");
+    song_list = insert_front(song_list, "Mr. Brightside", "The Killers");
+    song_list = insert_front(song_list, "Lift", "Radiohead");
+    song_list = insert_front(song_list, "Champagne Supernova", "Oasis");
+    song_list = insert_front(song_list, "American Idiot", "Green Day");
+    song_list = insert_front(song_list, "Lift", "Radiohead");
+    song_list = insert_front(song_list, "Otherside", "Red Hot Chili Peppers");
+    song_list = insert_front(song_list, "See You Again", "Tyler, The Creator");
+    song_list = insert_front(song_list, "Basket Case", "Green Day");
+    song_list = insert_front(song_list, "21 Guns", "Green Day");
+    song_list = insert_front(song_list, "Somebody Told Me", "The Killers");
+    song_list = insert_front(song_list, "Mr. Blue Sky", "Electric Light Orchestra");
+    song_list = insert_front(song_list, "On Melancholy Hill", "Gorillaz");
+    song_list = insert_front(song_list, "Snow (Hey Oh)", "Red Hot Chili Peppers");
+    song_list = insert_front(song_list, "Clint Eastwood", "Gorillaz");
+    printf("List: ");
+    print_list(song_list);
+    printf("___________________________\nFreeing list...\n");
+
+    song_list = free_list(song_list);
+    printf("List: ");
+    print_list(song_list);
+
+    printf("___________________________\nAdding items in order...\n");
     song_list = insert_ordered(song_list, "Runaway", "Kanye West");
     song_list = insert_ordered(song_list, "Coldest Winter", "Kanye West");
     song_list = insert_ordered(song_list, "Saint Pablo", "Kanye West");
@@ -27,6 +55,6 @@ int main() {
     song_list = insert_ordered(song_list, "Snow (Hey Oh)", "Red Hot Chili Peppers");
     song_list = insert_ordered(song_list, "Clint Eastwood", "Gorillaz");
 
-    printf("Result: ");
+    printf("List: ");
     print_list(song_list);
 }
