4d10dfd12ec1ad4c9b0da5c3838a27ddd86c6057
asultan00 <asultan00@stuy.edu>
2019-11-03 17:27:58 -0500

    added instructions for library

--- list.c
+++ list.c
@@ -14,6 +14,18 @@ find and return a pointer to the first song of an artist based on artist name [c
 Return a pointer to random element in the list. [completed]
 remove a single specified node from the list [completed]
 free the entire list [completed]
+
+DO NOT WORK ON THIS PART UNTIL YOUR LINKED LIST IS COMPLETE
+Then create your array of linked lists for the full program to have the following functions:
+Add song nodes.
+Search for a song given song and artist name (return a pointer).
+Search for an artist.
+Print out all the entries under a certain letter.
+Print out all the songs of a certain artist
+Print out the entire library.
+Shuffle - print out a series of randomly chosen songs.
+Delete a song
+Clear the library.
 --- INSTRUCTIONS --- */
 
 void print_list(struct song* list) {
